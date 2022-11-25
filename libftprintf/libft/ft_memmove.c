8057d8267666fc0de7eb688654f6a4d2d1cbf363
Raphael TISSERAND <rtissera@e2r4p3.clusters.42paris.fr>
2022-11-25 13:47:01 +0100

    libftprintf

---
+++ libftprintf/libft/ft_memmove.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rtissera <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/08 14:13:27 by rtissera          #+#    #+#             */
+/*   Updated: 2022/11/21 10:39:06 by rtissera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+	char	*s;
+	char	*d;
+
+	i = 0;
+	s = (char *)src;
+	d = (char *)dest;
+	if (d > s)
+	{
+		while (n-- > 0)
+			d[n] = s[n];
+	}
+	else
+	{
+		while (i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	return (dest);
+}
