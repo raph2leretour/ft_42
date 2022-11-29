cb243445cf08cd4ce5009e21a72ee4d2b55dafe5
Raphael TISSERAND <rtissera@e2r7p14.clusters.42paris.fr>
2022-11-29 17:56:28 +0100

    premier commit printf

---
+++ libftprintf/libft/ft_memcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rtissera <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/08 14:08:50 by rtissera          #+#    #+#             */
+/*   Updated: 2022/11/26 12:22:09 by rtissera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+	char	*s;
+	char	*d;
+
+	i = 0;
+	if (!dest && !src)
+		return (NULL);
+	s = (char *)src;
+	d = (char *)dest;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dest);
+}
---
+++ libftprintf/libft/ft_memmove.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rtissera <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/08 14:13:27 by rtissera          #+#    #+#             */
+/*   Updated: 2022/11/26 11:35:30 by rtissera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+	char	*s;
+	char	*d;
+
+	i = 0;
+	if (!dest && !src)
+		return (NULL);
+	s = (char *)src;
+	d = (char *)dest;
+	if (d > s)
+	{
+		while (n-- > 0)
+			d[n] = s[n];
+	}
+	else
+	{
+		while (i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	return (dest);
+}
