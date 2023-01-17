3beff0da21d67e056af5717a1b3d82c1c8eb885d
Raphael TISSERAND <rtissera@e2r8p12.clusters.42paris.fr>
2023-01-17 18:55:25 +0100

    bouyah

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rtissera <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 16:46:31 by rtissera          #+#    #+#             */
-/*   Updated: 2023/01/17 16:32:50 by rtissera         ###   ########.fr       */
+/*   Updated: 2023/01/17 18:25:24 by rtissera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,5 @@ char	*read_line(int	fd)
 char	*get_next_line(int fd)
 {
 	int			i;
-	int			j;
-	char		line[2048];
-	void		*buff;
-
-	if (!fd || (BUFFER_SIZE <= 0))
-		return (NULL);
+	static char	stash[1024];
 }
