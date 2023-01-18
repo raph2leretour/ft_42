09201e9d6c613daa6077685fc6e2a7ada7aa87d7
Raphael TISSERAND <rtissera@e2r12p5.clusters.42paris.fr>
2023-01-18 17:59:50 +0100

    gnl real start

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,18 +6,42 @@
 /*   By: rtissera <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 16:46:31 by rtissera          #+#    #+#             */
-/*   Updated: 2023/01/17 18:25:24 by rtissera         ###   ########.fr       */
+/*   Updated: 2023/01/18 17:57:13 by rtissera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*read_line(int	fd)
-{
-}
-
 char	*get_next_line(int fd)
 {
 	int			i;
+	int			j;
+	void		*buff;
+	char		*line;
 	static char	stash[1024];
+
+	if ((fd == -1) || (BUFFER_SIZE <= 0))
+		return (NULL);
+	i = 0;
+	while (stash[i] != '\n')
+	{
+		if (read(fd, buff, BUFFER_SIZE) == -1)
+			return (NULL);
+		j = 0;
+		while ((buff[j + 1] != '\n') && buff[j] && stash[i])
+		{
+			stash[i] = buff[j];
+			j++;
+			i++;
+		}
+	}
+	line = malloc(sizeof(char) * (i + 1));
+	j = 0;
+	while (j < i)
+	{
+		line[j] = stash[j];
+		j++;
+	}
+	line[j] = '\0';
+	return (line);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: rtissera <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 16:47:23 by rtissera          #+#    #+#             */
-/*   Updated: 2023/01/17 15:28:34 by rtissera         ###   ########.fr       */
+/*   Updated: 2023/01/18 17:58:08 by rtissera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
 
 # include <unistd.h>
 # include <stdlib.h>
