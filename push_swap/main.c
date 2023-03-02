bd9df2dab0057baecd552cdac22d85a6f29a25ac
Raphael TISSERAND <rtissera@e2r10p22.clusters.42paris.fr>
2023-03-02 18:22:22 +0100

    push_swap atoi_list

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: rtissera <rtissera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 14:02:56 by rtissera          #+#    #+#             */
-/*   Updated: 2023/03/01 16:29:17 by rtissera         ###   ########.fr       */
+/*   Updated: 2023/03/02 11:02:36 by rtissera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,18 @@
 
 int	main(int argc, char **argv)
 {
-	int	boool;
+	int		boool;
+	t_list	stocka;
 
-	// Check if args are valid
-	if (argc < 1)
+	if (argc < 2)
 		return (0);
+	// Check if args are valid
 	boool = checkarg(argv);
 	if (boool == 1)
 		return (ft_putchar_fd("Error\n", 1));
 	// Convert args into stocka
+	stocka = atoi_list(argv);
 	// Sort stocka
-	push_swap(t_list stocka);
+	push_swap(stocka);
 	return (0);
 }
--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: rtissera <rtissera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 14:03:00 by rtissera          #+#    #+#             */
-/*   Updated: 2023/03/01 16:20:59 by rtissera         ###   ########.fr       */
+/*   Updated: 2023/03/02 16:30:39 by rtissera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,42 @@
 
 int	checkarg(char **argv)
 {
+	int	i;
+	int	j;
+
+	i = 1;
+	while (argv[i])
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (ft_isdigit(argv[i][i]) != 1)
+			return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+t_list	*atoi_list(char **argv)
+{
+	int		i;
+	t_list	*stocka;
+	t_list	*head;
+
+	i = 1;
+	head = ft_lstnew(atoi(argv[i]));
+	stocka = head->next;
+	while (argv[i])
+	{
+		stocka = ft_lstnew(atoi(argv[i]));
+		stocka = stocka->next;
+		i++;
+	}
+	return (head);
 }
 
 int	push_swap(t_list *stocka)
 {
-	t_list	*stockb;
 }
