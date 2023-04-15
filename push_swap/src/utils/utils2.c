234ce348a17272c52b911358a129e5e3e2e32a43
Raph <rtissera@student.42.fr>
2023-04-15 17:16:49 +0200

    push_swap ok

--- push_swap/src/utils/utils2.c
+++ push_swap/src/utils/utils2.c
@@ -6,7 +6,7 @@
 /*   By: raphael <raphael@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 16:44:19 by rtissera          #+#    #+#             */
-/*   Updated: 2023/04/15 13:54:08 by raphael          ###   ########.fr       */
+/*   Updated: 2023/04/15 17:05:38 by raphael          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,14 +74,14 @@ void	convertnumb(int argc, t_list **stocka)
 	int		min;
 	t_list	*head;
 
-	i = -1;
+	i = 0;
 	head = *stocka;
 	min = ft_min(INT_MIN, *stocka);
-	while (i++ < argc)
+	while (i < argc)
 	{
 		if ((*stocka)->content == min)
 		{
-			(*stocka)->numb = i;
+			(*stocka)->numb = i++;
 			min = ft_min(min, head);
 		}
 		*stocka = (*stocka)->next;
