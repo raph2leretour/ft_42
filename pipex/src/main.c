fb07af1bf39967a7e005de2414cc260c0f7c4493
Raphael TISSERAND <rtissera@e2r6p18.clusters.42paris.fr>
2023-05-27 19:26:33 +0200

    makefile ok

--- pipex/src/main.c
+++ pipex/src/main.c
@@ -6,7 +6,16 @@
 /*   By: rtissera <rtissera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 14:38:45 by rtissera          #+#    #+#             */
-/*   Updated: 2023/05/26 14:38:50 by rtissera         ###   ########.fr       */
+/*   Updated: 2023/05/27 16:20:20 by rtissera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/pipex.h"
+
+int	main(int argc, char **argv)
+{
+	(void) argc;
+	(void) argv;
+	ft_putstr_fd("Hello, world!\n", 1);
+	return (0);
+}
