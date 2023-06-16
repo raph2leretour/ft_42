ed1fde8afd16e15671727017dbf3745a74022f66
Raphael TISSERAND <rtissera@paul-f4Br3s4.clusters.42paris.fr>
2023-06-16 17:09:35 +0200

    fd issues

--- 2nd circle/pipex/src/main.c
+++ 2nd circle/pipex/src/main.c
@@ -6,7 +6,7 @@
 /*   By: rtissera <rtissera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 13:57:53 by rtissera          #+#    #+#             */
-/*   Updated: 2023/06/15 18:39:25 by rtissera         ###   ########.fr       */
+/*   Updated: 2023/06/16 16:59:11 by rtissera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,11 @@ void	execificator(char *cmd, char **env)
 
 void	child_process(int f1, int end[2], char **av, char **env)
 {
+	ft_printf("f1: %d\nend[2]: %d\n", f1, end[1]);
 	dup2(f1, 0);
+	ft_printf("f1: %d\nend[2]: %d\n", f1, end[1]);
 	dup2(end[1], 1);
+	ft_printf("f1: %d\nend[2]: %d\n", f1, end[1]);
 	close(end[0]);
 	execificator(av[2], env);
 }
@@ -41,7 +44,9 @@ void	parent_process(int f2, int end[2], char **av, char **env)
 {
 	int	status;
 
+	// ft_printf("f2: %d\nend[2]: %d\n", f2, end[0]);
 	waitpid(-1, &status, 0);
+	ft_printf("waitpid check");
 	dup2(f2, 1);
 	dup2(end[0], 0);
 	close(end[1]);
@@ -71,7 +76,18 @@ int	main(int ac, char **av, char **env)
 	if (ac != 5)
 		return (ft_printf("Wrong argument count\n"), -1);
 	f1 = open(av[1], O_RDONLY, 0777);
+	if (f1 == -1)
+	{
+		perror("Error");
+		exit(-1);
+	}
 	f2 = open(av[4], O_CREAT | O_RDWR | O_TRUNC, 0777);
+	if (f2 == -1)
+	{
+		perror("Error");
+		close(f1);
+		exit(-1);
+	}
 	pipex(f1, f2, av, env);
 	close(f1);
 	close(f2);
