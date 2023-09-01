5e6420e5accc31564ef7f39f1eede1848d447a1d
Raphael TISSERAND <rtissera@paul-f4Ar6s3.clusters.42paris.fr>
2023-09-01 07:03:55 +0200

    im back bitch:

--- 2nd circle/so_long/src/check_map.c
+++ 2nd circle/so_long/src/check_map.c
@@ -3,23 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   check_map.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: raphael <raphael@student.42.fr>            +#+  +:+       +#+        */
+/*   By: rtissera <rtissera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 18:28:27 by rtissera          #+#    #+#             */
-/*   Updated: 2023/06/28 17:49:08 by raphael          ###   ########.fr       */
+/*   Updated: 2023/09/01 07:03:05 by rtissera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-// bool	check_format(char *map_path)
-// {
-// }
+bool	check_format(char *map_path)
+{
+	unsigned int	x;
+	unsigned int	y;
+
+	x = 0;
+	y = 0;
+}
 
 // bool	check_endable(char *map_path)
 // {
 // }
 
-// bool	check_map(char *map_path)
-// {
-// }
+bool	check_map(char *map_path)
+{
+}
