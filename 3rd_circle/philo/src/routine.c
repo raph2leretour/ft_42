0884b20d73bf52a66540c19d6375b1f513bce098
Raphael TISSERAND <rtissera@made-f0Cr10s3.clusters.42paris.fr>
2023-10-18 12:32:40 +0200

    routine modified

--- 3rd_circle/philo/src/routine.c
+++ 3rd_circle/philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: rtissera <rtissera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 14:50:19 by rtissera          #+#    #+#             */
-/*   Updated: 2023/10/17 19:13:33 by rtissera         ###   ########.fr       */
+/*   Updated: 2023/10/18 12:31:55 by rtissera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,13 +42,13 @@ void	eating(t_philo *philo)
 	pthread_mutex_unlock(&philo->data->stop_m);
 	pthread_mutex_lock(&philo->meal_m);
 	pthread_mutex_lock(&philo->data->eat_m);
+	ft_print("is eating", philo);
 	philo->nb_meals++;
 	if (philo->nb_meals == philo->data->nb_philo_eat)
 		philo->data->eat_v++;
 	philo->time_last_meal = get_time_in_ms();
 	pthread_mutex_unlock(&philo->meal_m);
 	pthread_mutex_unlock(&philo->data->eat_m);
-	ft_print("is eating", philo);
 	ft_usleep(philo->data->time_to_eat, philo->data);
 	throw_forks(philo);
 }
@@ -71,7 +71,8 @@ void	philo_routine(t_philo *philo)
 		return ;
 	pthread_mutex_unlock(&philo->data->stop_m);
 	ft_print("is thinking", philo);
-	ft_usleep(philo->data->time_to_eat - philo->data->time_to_sleep, \
+	ft_usleep((philo->data->time_to_eat > philo->data->time_to_sleep) \
+		* (philo->data->time_to_eat - philo->data->time_to_sleep) + 125, \
 		philo->data);
 	pthread_mutex_lock(&philo->data->stop_m);
 }
@@ -99,8 +100,8 @@ void	*routine(void *philou)
 	pthread_mutex_lock(&philo->meal_m);
 	philo->time_last_meal = get_time_in_ms();
 	pthread_mutex_unlock(&philo->meal_m);
-	if (philo->id % 2 != 0)
-		ft_usleep(philo->data->time_to_eat, philo->data);
+	if (philo->id & 1)
+		ft_usleep(100, philo->data);
 	pthread_mutex_lock(&philo->data->stop_m);
 	while (!philo->data->stop_v)
 	{
