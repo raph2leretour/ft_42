e98fcdcb8af3b12c0a99b5961f9a6b8efef56813
Raphael TISSERAND <rtissera@bess-f2r3s4.clusters.42paris.fr>
2024-02-09 22:29:45 +0100

    cpp01-> ex02 OK

--- 4th_circle/piscineCpp/cpp01/ex02/src/main.cpp
+++ 4th_circle/piscineCpp/cpp01/ex02/src/main.cpp
@@ -6,23 +6,25 @@
 /*   By: rtissera <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 19:18:20 by rtissera          #+#    #+#             */
-/*   Updated: 2024/02/09 19:26:46 by rtissera         ###   ########.fr       */
+/*   Updated: 2024/02/09 22:28:35 by rtissera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostream>
 
 int	main( void ) {
 
 	std::string	string = "HI THIS IS BRAIN";
-	std::string*	stringPtr = &string;
+	std::string*	stringPTR = &string;
 	std::string&	stringREF = string;
 
-	std::cout << &string << std::endl;
-	std::cout << stringPTR << std::endl;
-	std::cout << stringREF << std::endl;
+	std::cout << "string: " << &string << std::endl;
+	std::cout << "stringPTR: " << stringPTR << std::endl;
+	std::cout << "stringREF: " << &stringREF << std::endl;
 
-	std::cout << string << std::endl;
-	std::cout << *stringPTR << std::endl;
-	std::cout << stringREF << std::endl;
+	std::cout << "string: " << string << std::endl;
+	std::cout << "stringPTR: " << *stringPTR << std::endl;
+	std::cout << "stringREF: " << stringREF << std::endl;
 
 	return 0;
 }
