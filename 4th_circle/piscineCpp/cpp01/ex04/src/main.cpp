06f69584932497b1e1e61302de264fc96580ec0f
Raphael TISSERAND <rtissera@bess-f2r5s12.clusters.42paris.fr>
2024-02-14 19:37:45 +0100

    cpp01-> ex04 mid

--- 4th_circle/piscineCpp/cpp01/ex04/src/main.cpp
+++ 4th_circle/piscineCpp/cpp01/ex04/src/main.cpp
@@ -6,18 +6,38 @@
 /*   By: rtissera <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 14:21:11 by rtissera          #+#    #+#             */
-/*   Updated: 2024/02/11 17:06:15 by rtissera         ###   ########.fr       */
+/*   Updated: 2024/02/14 19:14:26 by rtissera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "BetterThanReplace.hpp"
+#include <iostream>
+#include <cstring>
+
+std::string	getFile( std::ifstream ifs ) {
+
+	std::string	file;
+
+	for ( ;; ) {
+
+		std::getline( ifs, file );
+	}
+
+	return file;
+}
+
+std::string	replace( std::string file ) {}
 
 int	main( int argc, char ** argv ) {
 
 	if ( argc == 3 ) {
 
-		BetterThanReplace	myReplace = BetterThanReplace( argv[1], argv[2], argv[3] );
-		myReplace::myReplace();
+		std::ifstream	ifs( argv[ 1 ] );
+		if ( !ifs.is_open() ) {
+
+			std::cout << "Error opening file" << std::endl;
+		}
+		std::string	file = getFile( ifs );
+		file = replace( file );
 	}
 	return 0;
 }
