5cdf9210fb7d66c98ac62650cfae1560977b30d3
Raphael TISSERAND <rtissera@paul-f4Br6s3.clusters.42paris.fr>
2024-07-26 18:15:52 +0200

    cpp05 main not OK

--- 5th_circle/piscineCpp02/cpp05/ex00/src/main.cpp
+++ 5th_circle/piscineCpp02/cpp05/ex00/src/main.cpp
@@ -6,11 +6,40 @@
 /*   By: rtissera <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 13:52:08 by rtissera          #+#    #+#             */
-/*   Updated: 2024/07/26 13:52:48 by rtissera         ###   ########.fr       */
+/*   Updated: 2024/07/26 18:13:03 by rtissera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	main( void ) {
+
+	try {
+
+		Bureaucrat b1( "Pablo", 1 );
+		std::cout << b1;
+		b1.incrementGrade();
+	} catch (const std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	try {
+		Bureaucrat b2;
+		std::cout << b2;
+		b2.decrementGrade();
+	} catch (const std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	try {
+		Bureaucrat b3("Pedro", 160);
+	} catch (const std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	try {
+		Bureaucrat b4("Hildegarde", -5);
+	} catch (const std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
 
 	return 0;
 }
