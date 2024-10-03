972c42519b00ec4fc140e2b923403b64ad4c2a0f
Raph <rtissera@student.42.fr>
2024-10-03 11:24:00 +0200

    plus rien

--- 5th_circle/piscineCpp02/cpp05/ex00/inc/Bureaucrat.hpp
+++ 5th_circle/piscineCpp02/cpp05/ex00/inc/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: rtissera <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 13:55:04 by rtissera          #+#    #+#             */
-/*   Updated: 2024/07/26 18:08:14 by rtissera         ###   ########.fr       */
+/*   Updated: 2024/10/03 11:22:50 by raphael          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,51 +26,7 @@
 /******************************************************************************/
 /*   CLASSES                                                                  */
 /******************************************************************************/
-class Bureaucrat {
-
-private:
-	static st::string const	_defaultName = "Gedeon";
-	static int const		_highestGrade = 1;
-	static int const		_lowestGrade = 150;
-
-	std::string const		_name;
-	int						_grade;
-
-protected:
-
-public:
-	Bureaucrat( void );
-	Bureaucrat( std::string name, size_t grade );
-	Bureaucrat( Bureacrat const & src );
-
-	virtual ~Bureaucrat( void );
-
-	// Operators
-	Bureaucrat&				operator=( Bureacrat const & rhs );
-
-	// Accessors
-	std::string				getName( void ) const;
-	size_t					getGrade( void ) const;
-
-	// Functions
-	void					increaseGrade( void );
-	void					decreaseGrade( void );
-
-	// Throw stuff
-	class GradeTooHighException : public std::exception {
-
-		public:
-			virtual const char* what() const throw();
-	};
-	class GradeTooLowException : public std::exception {
-
-		public:
-			virtual const char* what() const throw();
-	};
-
-};
 
 /******************************************************************************/
 /*   REDIRECTION OPERATOR                                                     */
 /******************************************************************************/
-std::ostream&				operator<<( std::ostream& o, Bureaucrat const & rhs );
--- 5th_circle/piscineCpp02/cpp05/ex00/src/class/Bureaucrat.cpp
+++ 5th_circle/piscineCpp02/cpp05/ex00/src/class/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: rtissera <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 14:00:41 by rtissera          #+#    #+#             */
-/*   Updated: 2024/07/26 17:05:49 by rtissera         ###   ########.fr       */
+/*   Updated: 2024/10/03 11:23:28 by raphael          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,93 +18,23 @@
 /******************************************************************************/
 /*   CONSTRUCTORS / DESTRUCTORS                                               */
 /******************************************************************************/
-Bureaucrat::Bureaucrat( void ) : _name( _defaultName ), _grade( _lowestGrade ) {}
-
-Bureaucrat::Bureaucrat( std::string name, int grade ) : _name( name ) {
-
-	if ( grade < _highestGrade ) {
-
-		throw Bureaucrat::GradeTooHighException();
-	} else if ( grade > _lowestGrade ) {
-
-		throw Bureaucrat::GradeTooLowException();
-	} else {
-
-		_grade = grade;
-	}
-}
-
-Bureaucrat::Bureaucrat( Bureaucrat const & src ) : _name( src._name ), _grade( src.grade ) {}
-
-Bureaucrat::~Bureaucrat( void ) {}
 
 /******************************************************************************/
 /*   ASSIGNATION OPERATOR                                                     */
 /******************************************************************************/
-Bureaucrat&	Bureaucrat::operator=( Bureaucrat const & rhs ) {
-
-	if ( this != &rhs ) {
-
-		*const_cast<std::string*>(&_name) = rhs._name;
-		_grade = rhs._grade;
-	}
-
-	return *this;
-}
 
 /******************************************************************************/
 /*   ACCESSORS                                                                */
 /******************************************************************************/
-std::string	Bureaucrat::getName( void ) const {
-
-	return _name;
-}
-
-std::string	Bureaucrat::getGrade( void ) const {
-
-	return _Grade;
-}
 
 /******************************************************************************/
 /*   FUNCTIONS                                                                */
 /******************************************************************************/
-void	Bureaucrat::increaseGrade( void ) {
-
-	if ( _grade == _lowestGrade ) {
-
-		throw Bureaucrat::GradeTooHighException();
-	}
-	++_grade;
-}
-
-void	Bureaucrat::decreaseGrade( void ) {
-
-	if ( _grade == _highestGrade ) {
-
-		throw Bureaucrat::GradeTooLowException();
-	}
-	--_grade;
-}
 
 /******************************************************************************/
 /*   THROW STUFF                                                              */
 /******************************************************************************/
-const char*	Bureaucrat::GradeTooHighException::what() const throw() {
-
-	return "Grade too high";
-}
-
-const char*	Bureaucrat::GradeTooLowException::what() const throw() {
-
-	return "Grade too Low";
-}
 
 /******************************************************************************/
 /*   REDIRECTION OPERATOR                                                     */
 /******************************************************************************/
-std::ostream&	operator<<( std::ostream& o, Bureaucrat const & rhs ) {
-
-	o << rhs.getName() << ", bureaucrat grade " << rhs.getGrade() << ".";
-
-	return o;
-}
